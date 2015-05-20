7733a4e74cfa67b20d0a890ab60197898cf7e91b
Alex Poms <alexpoms@gmail.com>
2015-05-20 10:42:59 -0700

    Out files based on name not full arg

--- scripts/pbs/main.sh
+++ scripts/pbs/main.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 test_args=(single_d row_2_d row_4_d row_8_d row_16_d row_32_d)
-test_args=("${test_args[@]/%/:lasso_mn_d_1}" )
 out_files=("${test_args[@]/%/}" )
+test_args=("${test_args[@]/%/:lasso_mn_d_1}" )
 test_config=(
     "-l nodes=1:k40:ppn=1 -l walltime=24:00:00"
     "-l nodes=1:k40x4:ppn=2 -l walltime=24:00:00"
