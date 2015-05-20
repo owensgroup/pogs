9e4fa367b0bbf60d8ed1718fd6fc296d7aa15005
Alex Poms <alexpoms@gmail.com>
2015-05-20 08:15:43 +0000

    Rename file output

--- scripts/pbs/torque.sh
+++ scripts/pbs/torque.sh
@@ -19,5 +19,5 @@ export MV2_USE_CUDA=1
 cd ~/repos/pogs
 source .env/bin/activate
 cd ~/repos/pogs/scripts
-python benchmark.py --spec ref_spec.json --plan ${test_arg} --results pbs/results/${test_file}_results.json
+python benchmark.py --spec ref_spec.json --plan ${test_arg} --results pbs/results/${result_file}_results.json
 deactivate
