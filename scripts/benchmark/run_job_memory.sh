8de9b1d8844c77973e65bdf4eae3f855bfcd481e
Alex Poms <alexpoms@gmail.com>
2015-07-14 19:34:23 -0400

    Pass along all args instead of trying to pick out

--- scripts/benchmark/run_job_memory.sh
+++ scripts/benchmark/run_job_memory.sh
@@ -3,9 +3,6 @@
 #PBS -M abpoms@ucdavis.edu
 #PBS -m abe
 
-result_file=$1
-test_arg=$2
-
 cd ~/repos/pogs/scripts/benchmark
 source modules_mvapich_gdr.sh
 
@@ -13,5 +10,5 @@ cd ~/repos/pogs
 source .env/bin/activate
 
 cd ~/repos/pogs/scripts/benchmark
-python benchmark.py --spec ../ref_spec.json --plan ${test_arg} --results results/${result_file}
+python benchmark.py "$@"
 deactivate
