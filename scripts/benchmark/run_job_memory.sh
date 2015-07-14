f8b9926eb70d5994c4f595936405d018b25650cf
Alex Poms <alexpoms@gmail.com>
2015-07-14 19:24:47 -0400

    Need to load module before env load for PATH

--- scripts/benchmark/run_job_memory.sh
+++ scripts/benchmark/run_job_memory.sh
@@ -6,10 +6,12 @@
 result_file=$1
 test_arg=$2
 
+cd ~/repos/pogs/scripts/benchmark
+source modules_mvapich_gdr.sh
 
 cd ~/repos/pogs
 source .env/bin/activate
+
 cd ~/repos/pogs/scripts/benchmark
-source modules_mvapich_gdr.sh
 python benchmark.py --spec ../ref_spec.json --plan ${test_arg} --results results/${result_file}_results.json
 deactivate
