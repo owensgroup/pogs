513c92cfa5b1676c207ae1d7d480d0b1bda97eee
Alex Poms <alexpoms@gmail.com>
2015-07-14 15:24:01 -0400

    Update launch script for new directory structure

--- scripts/benchmark/run_job_memory.sh
+++ scripts/benchmark/run_job_memory.sh
@@ -6,10 +6,10 @@
 result_file=$1
 test_arg=$2
 
-source modules_mvapich_gdr.sh
 
 cd ~/repos/pogs
 source .env/bin/activate
 cd ~/repos/pogs/scripts/benchmark
-python benchmark.py --spec ref_spec.json --plan ${test_arg} --results results/${result_file}_results.json
+source modules_mvapich_gdr.sh
+python benchmark.py --spec ../ref_spec.json --plan ${test_arg} --results results/${result_file}_results.json
 deactivate
